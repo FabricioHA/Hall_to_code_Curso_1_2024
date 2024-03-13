08b7af81c85cac976e6ed5b078d6914bfa8f4fad
FabricioHA <fabricio.27767@gmail.com>
2024-03-13 15:20:05 -0300

    Update twelfth class files

--- Aula_12/TicTacToe/scripts/index.js
+++ Aula_12/TicTacToe/scripts/index.js
@@ -15,18 +15,6 @@ ou
 nomeDoObjeto.indexDoObjeto.indexDoIndexDoObjeto;
 */
 
-function toggleCurrentMove() //Tradução: alternar movimento atual
-{
-    if(game.currentMove == "X")
-    {
-        game.currentMove = "O"
-    }
-    else if(game.currentMove== "O")
-    {
-        game.currentMove = "X"
-    }
-}
-
 /*Nesse caso, criamos uma função chamada "getField(), onde em vez de atribuições de variaveis individuais, fazemos uma função para pegar determinado elemento
 do HTML de acordo com o número da sua classe secundária"*/
 function getField(fieldNumber) //esse parametro represennta o número correspondente do elemento HTML na sua classe.
@@ -36,11 +24,39 @@ function getField(fieldNumber) //esse parametro represennta o número correspond
     return $field; //Nesse caso, retornamos o valor "document.querySelector(".scenary-field-"+fieldNumber);", sendo ele valor da variavel $field
 }
 
+function toggleCurrentMove() //Tradução: alternar movimento atual
+{
+    if(game.currentMove == "X") 
+    {
+        game.currentMove = "O";
+    }
+    else if(game.currentMove== "O")
+    {
+        game.currentMove = "X";
+    }
+}
+
+function getWinner()
+{
+    const $fieldList = document.querySelectorAll(".scenary-field-big");
+     /*Nesse caso acima, em vez de um query selector convencional, capaz de armazenar apenas um valor 1:1, o querySelectorAll irá armazenar todos os
+     campos com a classe especificada, criando um array para armazena-los sequencialmente de cima para baixo*/
+
+     /*Com isso, temos a lista de todos os campos selecionados via a classe especificada.*/ 
+     
+     if($fieldList[0].textContent == $fieldList[1].textContent && $fieldList[1].textContent == $fieldList[2].textContent)
+     {
+        alert("foi :)")
+     }
+}
+
+getWinner();
+
 for (let index = 0; index <= 8; index++) //Agora criamos um loop para indexar cada field até que seja menor ou igual a 8
 {
     const $field = getField(index); //Depositamos o valor da função de acordo com sua numeração
 
-    $field.addEventListener("click", function()
+    $field.addEventListener("click", function() //Assim que o ekemento for clicado, ele será incrementado até o seu indice correspondente.
     {
         $field.textContent = game.currentMove; //Aqui pegamos um valor de um objeto transcrevemos ele no field
         toggleCurrentMove(); /*Aqui, quando o field for transcrito apartir do objeto game.currentMove, ele irá substituir seu valor de X para O, alternando
